a2d1903dde131182853a4c3d2660bfb3e0057e30
Mariane <marianemara@ufmg.br>
2024-06-10 12:08:21 -0300

    adicionando clear screen na main

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
+import os
 from calculadora_nsg import NSGCalculadora
 
-def main():
+def clear_screen():
+    # Verifica o sistema operacional e executa o comando correspondente
+    if os.name == 'nt':  # Para Windows
+        os.system('cls')
+    else:  # Para Unix/Linux/Mac
+        os.system('clear')
 
+def main():
     calculadora_nsg = NSGCalculadora()
 
     print("Qual a quantidade de matérias que você cursou este semestre?")
-
     numero_materias = int(input())
+    clear_screen() # Limpa a tela
 
     for i in range(numero_materias):
-        print("Digite o número de HORAS da {}º materia:" .format(i + 1))
+        print("Digite o número de HORAS da {}º matéria:".format(i + 1))
         horas = int(input())
-        print("Digite a NOTA tirada na {}º materia:" .format(i + 1))
+        print("Digite a NOTA tirada na {}º matéria:".format(i + 1))
         nota = int(input())
 
         calculadora_nsg.adiciona_materia(horas, nota)
+        clear_screen()  # Limpa a tela
 
     nsg = calculadora_nsg.calcula_nsg()
     print("Seu NSG nesse semestre foi: ", round(nsg, 2))
